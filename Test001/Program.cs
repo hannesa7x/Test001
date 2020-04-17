252aa14a1278cd4bea971bbde7f54dd4c34fce50
Hanspeter Umilietti <umihan@rgtfo-me.it>
2020-04-17 20:31:58 +0200

    1.Commit

--- Test001/Program.cs
+++ Test001/Program.cs
@@ -10,6 +10,23 @@ namespace Test001
     {
         static void Main(string[] args)
         {
+            Titel("Dies ist ein Test");
+            Console.ReadKey();
         }
+        static void Titel(string titel)
+        {
+            Console.Clear();
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine(titel);
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("-");
+            }
+
+        }
+
     }
 }
